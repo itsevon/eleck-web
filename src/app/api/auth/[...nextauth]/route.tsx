31672b4f300828dede933b197d940c264398ea92
Evone <yfh910@gmail.com>
2023-12-17 00:59:09 +0900

    WIP

--- src/app/api/auth/[...nextauth]/route.tsx
+++ src/app/api/auth/[...nextauth]/route.tsx
@@ -2,13 +2,17 @@ import NextAuth from 'next-auth'
 import CognitoProvider from 'next-auth/providers/cognito'
 
 const handler = NextAuth({
+  
   secret: process.env.NEXTAUTH_SECRET,
   providers: [
     CognitoProvider({
       clientId: process.env.COGNITO_CLIENT_ID ?? '',
       clientSecret: process.env.COGNITO_CLIENT_SECRET ?? '',
       issuer: process.env.COGNITO_ISSUER ?? '',
-      checks: 'nonce',
+      checks: 'nonce', 
+      // httpOptions: {
+      //   timeout: 40000,
+      // },
     }),
   ],
 })
